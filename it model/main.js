dbb36dbaf48cf74b331b898e56357f7b232d4bd4
Evgeny Kuizhev <ekuizhev@yandex.ru>
2018-07-11 13:56:45 +0300

    изменен порядок вызово методов

--- it model/main.js
+++ it model/main.js
@@ -29,16 +29,42 @@ const simuleteDays = (count, company) => {
     // сначала директор получает и распределяет проекты
     company.director.getProjects();
     count--;
+    company.director.hireEmployees();
+    company.director.distributeProjects();
+    company.director.reduceDay();
+    company.director.fireWorker();
+    company.director.getProjects();
+    count--;
     while (count--) {
         // на следующий день директор нанимает разрабов для вчерашних проетов
+        company.director.distributeProjects();
         company.director.hireEmployees();
         company.director.getProjects();
-        company.director.distributeProjects();
         company.director.reduceDay();
         company.director.fireWorker();
     }
+    // getAllEmployees(company);
     return company.director.getStatistik();
 }
 
-const days = 26;
+const days = 219;
+
 console.log(simuleteDays(days, apple));
+
+
+
+
+function getAllEmployees(company) {
+    console.log('-----------FREE EMPLOYEES-------------');
+    console.log(company.director.webDepartment.freeEmployees.length);
+    console.log(company.director.mobileDepartment.freeEmployees.length);
+    console.log(company.director.testDepartment.freeEmployees.length);
+    console.log('-----------FIRED EMPLOYEES-------------');
+    console.log(company.director.webDepartment.firedEmployees.length);
+    console.log(company.director.mobileDepartment.firedEmployees.length);
+    console.log(company.director.testDepartment.firedEmployees.length);
+    console.log('-----------BUSY EMPLOYEES-------------');
+    console.log(company.director.webDepartment.busyEmployees.length);
+    console.log(company.director.mobileDepartment.busyEmployees.length);
+    console.log(company.director.testDepartment.busyEmployees.length);
+}
